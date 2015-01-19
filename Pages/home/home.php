104b56ea0f86605aa3a59a1ab529dce3940fe9ff
152cooperR <nrtheprogrammer@gmail.com>
2015-01-19 00:03:29 -0800

    Added BootStrap

--- Pages/home/home.php
+++ Pages/home/home.php
@@ -1,50 +1,99 @@
 <html>
 <head>
-    <link href="/Julies Website/assets/css/main.css" rel="stylesheet" type="text/css">
-    <link href="this.css" rel="stylesheet" type="text/css">
-    <div id="veryTop">
-        <a>Julies Personal Assistant Service</a>
-    </div>
+    <meta charset="utf-8">
+    <meta name="veiwport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="\Julies_Website\css\bootstrap.min.css"/>
+    <link rel="stylesheet" href="\Julies_Website\css\bootstrap-responsive.min.css">
 </head>
+<div class="jumbotron">
+        <div class="container">
+
+            <h2>Julies Personal Assistant Service</h2>
+
+        </div>
+</div>
 <body>
 
-<div class="navBar">
-<ul>
-    <li><a href="/Julies Website/Pages/home/home.php" id="home">Home</a></li>
-    <li><a href="/Julies Website/Pages/PersonalCare/personalCare.php" id="personalCare" >Personal Care</a></li>
-    <li><a href="/Julies Website/Pages/Org/org.php" id="organization" >Organization</a></li>
-    <li><a href="/Julies Website/Pages/Moving/moving.php" id="moving" >Moving In/Out</a></li>
-    <li><a href="/Julies Website/Pages/Reminding/remind.php" id="remind" >Reminder services</a></li>
-    <li><a href="/Julies Website/Pages/Entertainment/ent.php" id="entertaining" >Entertaining</a></li>
-    <li><a href="/Julies Website/Pages/Shopping/shopping.php" id="shopping" >Personal shopping</a></li>
-    <li><a href="/Julies Website/Pages/Sched/sched.php" id="scheduling" >Schedule Services</a></li>
-    <li><a href="/Julies Website/Pages/Pricing/pricing.php" id="pricing" >Pricing</a></li>
-</ul>
-
-    <script language="JavaScript" type="text/javascript" >
-        <!-- Hide JavaScript
 
 
+<div class="container">
 
-
-
-        -->
-    </script>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#"></a>
+            </div>
+            <div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Entertainment</a></li>
+                    <li><a href="#">Moving</a></li>
+                    <li><a href="#">Organization</a></li>
+                    <li><a href="#">Personal Care</a></li>
+                    <li><a href="#">Pricing</a></li>
+                    <li><a href="#">Reminder Services</a></li>
+                    <li><a href="#">Scheduling</a></li>
+                    <li><a href="#">Shopping</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
 </div>
-<div class="contentWrapper">
+
+<div class="container">
+
+
+        <div class="panel  panel-primary">
+
+            <div class="panel-heading">Welcome!</div>
+            <div class="panel-body">
+
+                <p>Welcome to Julies personal assistant website! Here we offer many services including:
+                <ul>
+                    <li><a href="#">Entertainment</a></li>
+                    <li><a href="#">Moving</a> </li>
+                    <li><a href="#">Organisation</a> </li>
+                    <li><a href="#">Personal Care</a></li>
+                    <li><a href="#">Reminder Services</a></li>
+                    <li><a href="#">Scheduling</a> </li>
+                    <li><a href="#">Shopping</a></li>
+                </ul>
+                To find out more about each of the services, click each of the links above.
+                </p>
+
+            </div>
+        </div>
+
 </div>
-    <div class="CB" ">
-    <h3>Welcome<br></h3>
-        <a>Welcome to Julies personal assistant website!</a>
+
+
+
+
+<div class="navbar navbar-fixed-bottom">
+    <div class="container">
+
+        <a>© Nathan Rhodes 2015. All Rights Reserved.</a>
     </div>
 
 
 </div>
-<div class="credit">
 
-    <a>© Nathan Rhodes 2015. All Rights Reserved.</a>
 
-</div>
+
+<script src="\js\jquery-1.11.2.min.js" type="text/javascript"></script>
+<script src="\js\bootstrap.min.js" type="text/javascript"></script>
 </body>
+
+<!--
+            <li><a href="/Julies Website/Pages/home/home.php">Home</a></li>
+            <li><a href="/Julies Website/Pages/PersonalCare/personalCare.php">Personal Care</a></li>
+            <li><a href="/Julies Website/Pages/Org/org.php">Organization</a></li>
+            <li><a href="/Julies Website/Pages/Moving/moving.php" >Moving In/Out</a></li>
+            <li><a href="/Julies Website/Pages/Reminding/remind.php" >Reminder services</a></li>
+            <li><a href="/Julies Website/Pages/Entertainment/ent.php" >Entertaining</a></li>
+            <li><a href="/Julies Website/Pages/Shopping/shopping.php" >Personal shopping</a></li>
+            <li><a href="/Julies Website/Pages/Sched/sched.php"  >Schedule Services</a></li>
+            <li><a href="/Julies Website/Pages/Pricing/pricing.php" >Pricing</a></li>
+-->
 </html>
