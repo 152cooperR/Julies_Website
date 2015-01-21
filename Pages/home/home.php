568815eb27089126323f1f1316edc0072b7cbe86
152cooperR <nrtheprogrammer@gmail.com>
2015-01-20 18:21:02 -0800

    Fixed JS for jqueery

--- Pages/home/home.php
+++ Pages/home/home.php
@@ -4,10 +4,8 @@
     <meta charset="utf-8">
     <meta name="veiwport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="\Julies_Website\css\bootstrap.min.css"/>
-    <link rel="stylesheet" href="\css\bootstrap-responsive.min.css">
-    <link rel="stylesheet" href="\css\bootstrap-theme.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap-responsive.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
 </head>
 <div class="jumbotron">
         <div class="container">
@@ -19,7 +17,7 @@
 <body>
 
 
-
+<!--
 <div class="container">
 
     <nav class="navbar navbar-inverse" >
@@ -39,10 +37,55 @@
                     <li><a href="#">Scheduling</a></li>
                     <li><a href="#">Shopping Services</a></li>
                 </ul>
+
+
             </div>
         </div>
     </nav>
 
+
+
+</div>
+-->
+
+<body>
+<div class="navbar-wrapper">
+    <div class="container">
+
+        <nav class="navbar navbar-inverse navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Project name</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Nav header</li>
+                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    </div>
 </div>
 
 <div class="container">
@@ -66,7 +109,10 @@
                 To find out more about each of the services, click each of the links above.
                 </p>
 
+
             </div>
+
+
         </div>
 
 </div>
@@ -106,6 +152,10 @@
     <h5>© Julies Personal Assistant Inc. 2015. All Rights Reserved. </h5>
 </div>
 
+<script src="/js/bootstrap.min.js"></script>
+<script src="/js/jquery-2.1.3.min.js"></script>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 </body>
 
 <!--
@@ -119,4 +169,5 @@
             <li><a href="/Julies Website/Pages/Sched/sched.php"  >Schedule Services</a></li>
             <li><a href="/Julies Website/Pages/Pricing/pricing.php" >Pricing</a></li>
 -->
+
 </html>
