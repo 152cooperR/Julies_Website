296ba313410285b38ae04cc01e060d91a80ef264
Nathan <nrtheprogrammer@gmail.com>
2015-01-20 16:59:37 -0800

    Updated home page

--- Pages/home/home.php
+++ Pages/home/home.php
@@ -1,9 +1,13 @@
 <html>
 <head>
+    <meta lang="en">
     <meta charset="utf-8">
     <meta name="veiwport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="\Julies_Website\css\bootstrap.min.css"/>
-    <link rel="stylesheet" href="\Julies_Website\css\bootstrap-responsive.min.css">
+    <link rel="stylesheet" href="\css\bootstrap-responsive.min.css">
+    <link rel="stylesheet" href="\css\bootstrap-theme.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 </head>
 <div class="jumbotron">
         <div class="container">
@@ -50,14 +54,14 @@
             <div class="panel-body">
 
                 <p>Welcome to Julies personal assistant website! Here we offer many services including:
-                <ul>
-                    <li><a href="#">Entertainment</a></li>
-                    <li><a href="#">Moving</a> </li>
-                    <li><a href="#">Organisation</a> </li>
-                    <li><a href="#">Personal Care</a></li>
-                    <li><a href="#">Reminder Services</a></li>
-                    <li><a href="#">Scheduling</a> </li>
-                    <li><a href="#">Shopping</a></li>
+                <ul class="list-group">
+                    <li class="list-group-item"><a href="#">Entertainment</a></li>
+                    <li class="list-group-item"><a href="#">Moving</a> </li>
+                    <li class="list-group-item"><a href="#">Organisation</a> </li>
+                    <li class="list-group-item"><a href="#">Personal Care</a></li>
+                    <li class="list-group-item"><a href="#">Reminder Services</a></li>
+                    <li class="list-group-item"><a href="#">Scheduling</a> </li>
+                    <li class="list-group-item"><a href="#">Shopping</a></li>
                 </ul>
                 To find out more about each of the services, click each of the links above.
                 </p>
@@ -66,23 +70,42 @@
         </div>
 
 </div>
+<div class="container">
 
 
+    <div class="panel  panel-primary">
 
+        <div class="panel-heading">Contact us:</div>
+        <div class="panel-body">
 
-<div class="navbar navbar-fixed-bottom">
-    <div class="container">
+            <p>Here at Jules Personal Assistant we are dedicated to helping you the, customer.
+            If you have any questions about the services we offer you can
+            <a href="mailto:nrtheprogrammer@gmail.com">email us</a>
+            , we will respond as soon as possible. </p>
 
-        <a>© Nathan Rhodes 2015. All Rights Reserved. </a>
+        </div>
     </div>
-
 
 </div>
 
 
 
-<script src="\js\jquery-1.11.2.min.js" type="text/javascript"></script>
-<script src="\js\bootstrap.min.js" type="text/javascript"></script>
+
+
+<div class="container">
+
+    <div class="alert alert-danger">
+        <p>Please note that this website is still a work in porgress,
+            if you find a bug please report it <a href="mailto:nrtheprogrammer@gmail.com">here</a>.</p>
+
+    </div>
+
+</div>
+
+<div class="container">
+    <h5>© Julies Personal Assistant Inc. 2015. All Rights Reserved. </h5>
+</div>
+
 </body>
 
 <!--
