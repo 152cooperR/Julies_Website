0e01554332d1214afc4603f5f5845e95874bb668
152cooperR <nrtheprogrammer@gmail.com>
2015-01-24 15:27:20 -0800

    Updated home page

--- Pages/home/home.php
+++ Pages/home/home.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <html>
 <head>
 
@@ -11,7 +12,7 @@
     </script>
     <meta lang="en">
     <meta charset="utf-8">
-    <meta content='width=device-width, initial-scale=1' name='viewport'/>
+    <meta content='width=device-width, initial-scale=1' name='viewport'>
 
     <!-- For linux and everything else in the world -->
     <link rel="stylesheet" href="/css/bootstrap.css"/>
@@ -220,4 +221,252 @@
 <script src="\Julies_Website\js\bootstrap.min.js"></script>
 </body>
 
+=======
+<html>
+<head>
+
+    <script type="text/javascript">
+        <!--
+        if (screen.width <= 699) {
+            document.location = "mobile/home.html";
+        }
+        //-->
+    </script>
+    <meta lang="en">
+    <meta charset="utf-8">
+    <meta content='width=device-width, initial-scale=1' name='viewport'/>
+
+    <!-- For linux and everything else in the world -->
+    <link rel="stylesheet" href="/css/bootstrap.css"/>
+    <link rel="stylesheet" href="/css/bootstrap-responsive.min.css"/>
+    <link rel="stylesheet" href="/css/bootstrap-theme.min.css"/>
+    <!-- For windows -_- -->
+
+    <link rel="stylesheet" href="\Julies_Website\css\bootstrap.min.css"/>
+    <link rel="stylesheet" href="\Julies_Website\css\bootstrap-responsive.min.css"/>
+    <link rel="stylesheet" href="\Julies_Website\css\bootstrap-theme.min.css"/>
+
+
+</head>
+
+
+
+<!--
+<div class="container">
+
+    <nav class="navbar navbar-inverse" >
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#"></a>
+            </div>
+            <div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Entertainment</a></li>
+                    <li><a href="#">Moving</a></li>
+                    <li><a href="#">Organization</a></li>
+                    <li><a href="#">Personal Care</a></li>
+                    <li><a href="#">Pricing</a></li>
+                    <li><a href="#">Reminder Services</a></li>
+                    <li><a href="#">Scheduling</a></li>
+                    <li><a href="#">Shopping Services</a></li>
+                </ul>
+
+
+            </div>
+        </div>
+    </nav>
+
+
+
+</div>
+-->
+
+<body>
+<div class="jumbotron">
+    <div class="container">
+
+        <h2>Julies Personal Assistant Service</h2>
+
+    </div>
+</div>
+<div class="navbar-wrapper">
+    <div class="container">
+
+        <nav class="navbar navbar-inverse">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Navigation</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Entertainment<span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Invitations / party</a></li>
+                                <li><a href="#">RSVP Coordination</a></li>
+                                <li><a href="#">Coordination with vendors</a></li>
+                                <li><a href="#">Grocery & Alcohol Shopping</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Moving<span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Hire a moving company</a></li>
+                                <li><a href="#">Day of move coordination</a></li>
+                                <li><a href="#">Pack, un-pack & organize</a></li>
+                                <li><a href="#">Stock house with supplies</a></li>
+                                <li><a href="#">Setup utilises accounts</a></li>
+                                <li><a href="#">Meet delivery people</a></li>
+                                <li><a href="#">Donate un-wanted items</a></li>
+
+                            </ul>
+                        </li>
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Organization<span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Nav header</li>
+                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Personal Care<span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Nav header</li>
+                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+
+                        <li><a href="#">Pricing</a></li>
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Scheduling<span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Nav header</li>
+                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+
+                        <li><a href="#">Reminder Services</a></li>
+
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Shopping Services<span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li class="divider"></li>
+                                <li class="dropdown-header">Nav header</li>
+                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    </div>
+</div>
+
+<div class="container">
+
+
+        <div class="panel  panel-primary">
+
+            <div class="panel-heading">Welcome!</div>
+            <div class="panel-body">
+
+                <p>Welcome to Julies personal assistant website! Here we offer many services including:
+                <ul class="list-group">
+                    <li class="list-group-item"><a href="#">Entertainment</a></li>
+                    <li class="list-group-item"><a href="#">Moving</a> </li>
+                    <li class="list-group-item"><a href="#">Organisation</a> </li>
+                    <li class="list-group-item"><a href="#">Personal Care</a></li>
+                    <li class="list-group-item"><a href="#">Reminder Services</a></li>
+                    <li class="list-group-item"><a href="#">Scheduling</a> </li>
+                    <li class="list-group-item"><a href="#">Shopping</a></li>
+                </ul>
+                To find out more about each of the services, click each of the links above.
+                </p>
+
+
+            </div>
+
+
+        </div>
+
+</div>
+<div class="container">
+
+
+    <div class="panel  panel-primary">
+
+        <div class="panel-heading">Contact us:</div>
+        <div class="panel-body">
+
+            <p>Here at Jules Personal Assistant we are dedicated to helping you the, customer.
+            If you have any questions about the services we offer you can
+            <a href="mailto:nrtheprogrammer@gmail.com">email us</a>
+            , we will respond as soon as possible. </p>
+
+        </div>
+    </div>
+
+</div>
+
+
+
+
+
+<div class="container">
+
+    <div class="alert alert-danger">
+        <p>Please note that this website is still a work in porgress,
+            if you find a bug please report it <a href="mailto:nrtheprogrammer@gmail.com">here</a>.</p>
+
+    </div>
+
+</div>
+
+<div class="container">
+    <h5>© Julies Personal Assistant Inc. 2015. All Rights Reserved. </h5>
+</div>
+<!-- For all os !Windows -->
+<script src="/js/jquery-2.1.3.min.js"></script>
+<script src="/js/bootstrap.min.js"></script>
+
+<!-- For windows -->
+
+<script src="\Julies_Website\js\jquery-2.1.3.min.js"></script>
+<script src="\Julies_Website\js\bootstrap.min.js"></script>
+</body>
+
+>>>>>>> Stashed changes
 </html>
